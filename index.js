fb999320968b0334c6ddebf0c3c3e0a66f87b8ed
zelda <shwarzbergzelda@gmail.com>
2026-06-08 15:49:43 -0400

    convert math, add screenshot of homepage

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
 const convertBtn = document.getElementById('convert-btn')
+const numberInput = document.getElementById('number-input')
+const length = document.getElementById('length')
+const volume = document.getElementById('volume')
+const mass = document.getElementById('mass')
 
 convertBtn.addEventListener('click', () => {
-    console.log('clicked')
+    if (numberInput.value === "") {
+        alert('Please enter a number')
+        return
+    }
+
+    const numberToConvert = Number(numberInput.value)
+
+    // convert length between meters and feet
+    const feet = (numberToConvert * 3.28084).toFixed(3)
+    const meters = (numberToConvert / 3.28084).toFixed(3)
+
+    length.textContent = `${numberToConvert} meters = ${feet} feet | ${numberToConvert} feet = ${meters} meters`
+
+
+    // convert volume between liters and gallons
+    const gallons = (numberToConvert * 0.264172).toFixed(3)
+    const liters = (numberToConvert / 0.264172).toFixed(3)
+
+    volume.textContent = `${numberToConvert} liters = ${gallons} gallons | ${numberToConvert} gallons = ${liters} liters`
+
+
+    // convert mass between kilograms and pounds
+    const pounds = (numberToConvert * 2.20462).toFixed(3)
+    const kilograms = (numberToConvert / 2.20462).toFixed(3)
+
+    mass.textContent = `${numberToConvert} kilograms = ${pounds} pounds | ${numberToConvert} pounds = ${kilograms} kilograms`
+
+    numberInput.value = ""
 })
